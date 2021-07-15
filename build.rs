f0155990a7f89308824ad4f30f1984dc3bebed56
baku <bahkhun@gmail.com>
2021-07-15 18:39:50 +0900

    Add submodule to build

--- build.rs
+++ build.rs
@@ -1,28 +1,13 @@
 use std::env;
-use std::path::Path;
 
-use git2::Repository;
 use cmake::Config;
 
 fn main() {
-    // libdivsufsort repository
-    let repo_url = "https://github.com/y-256/libdivsufsort";
-
     // output path
-    let out_path_str = env::var("OUT_DIR").unwrap();
-    let out_path = Path::new(&out_path_str);
-    let repo_path = out_path.join("libdivsufsort");
-
-    // clone repository
-    if !repo_path.exists() {
-        match Repository::clone(repo_url, repo_path.clone()) {
-            Ok(repo) => repo,
-            Err(e) => panic!("failed to clone: {}", e),
-        };
-    }
+    let out_path = env::var("OUT_DIR").unwrap();
 
     // cmake build
-    let dst = Config::new(repo_path)
+    let dst = Config::new("libdivsufsort")
         .define("BUILD_EXAMPLES", "OFF")
         .define("BUILD_SHARED_LIBS", "OFF")
         .define("BUILD_DIVSUFSORT64", "ON")
